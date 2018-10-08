b3cbde8dc48d46dd1eff13d6af5c59bbfc991269
Michael <m.pilyavskiy@gmail.com>
2018-10-08 21:57:41 +0300

    2a

--- mpl_RS5k_manager_control/mpl_RS5k_manager_control_functions.lua
+++ mpl_RS5k_manager_control/mpl_RS5k_manager_control_functions.lua
@@ -1,7 +1,7 @@
 -- @description RS5k_manager_control_functions
 -- @author MPL
 -- @website http://forum.cockos.com/member.php?u=70694
--- @version 1.01
+-- @version 1.02
 
   -----------------------------------------------------------------------   
   function SetGlobalParam(val, param, incr)
